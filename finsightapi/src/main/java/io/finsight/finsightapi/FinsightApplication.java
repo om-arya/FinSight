6c71da092326c08807b3ff8201aee9fff81ed563
om-arya <om.arya0577@gmail.com>
2024-06-23 18:43:43 -0400

    Create finsightapi

---
+++ finsightapi/src/main/java/io/finsight/finsightapi/FinsightApplication.java
@@ -0,0 +1,13 @@
+package io.finsight.finsightapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsightApplication.class, args);
+	}
+
+}
